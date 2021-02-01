93c213ea563084c21790f8da6c08092b8a80b928
David J. Gardner <djglxxii@outlook.com>
2021-02-01 15:58:26 -0500

    example

---
+++ WebFormsDataBinding/Pages/MyPage.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WebFormsDataBinding.Infrastructure;
+
+namespace WebFormsDataBinding.Pages
+{
+    public partial class MyPage : System.Web.UI.Page
+    {
+        protected MyPageModel Model { get; } = new MyPageModel();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                var provider = new CustomFormsValueProvider(ModelBindingExecutionContext);
+                TryUpdateModel(Model, provider);
+
+                if (ModelState.IsValid)
+                {
+                    // ModelState will be invalid due to trying to bind the value 'on' to IsMarried bool.
+                }
+            }
+
+            DataBind();
+        }
+    }
+}
---
+++ WebFormsDataBinding/Pages/MyPageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebFormsDataBinding.Pages
+{
+    public class MyPageModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public bool IsMarried { get; set; }
+    }
+}
