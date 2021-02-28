f711bceeb117e0eb7c93929468ac0b2ce583c900
David J. Gardner <djglxxii@outlook.com>
2021-02-28 15:32:29 -0500

    working show/hide passwordbox

--- AspNetWebForms/Controls/CustomPasswordBox.cs
+++ AspNetWebForms/Controls/CustomPasswordBox.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.UI;
+﻿using System.Linq.Expressions;
+using System.Text;
+using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
@@ -7,23 +9,39 @@ namespace AspNetWebForms.Controls
     public class CustomPasswordBox : WebControl
     {
         public string Password { get; set; }
+        public string ShowPasswordLocalKey { get; set; } = "Show Password";
 
         protected override void Render(HtmlTextWriter writer)
         {
             // <div>
             writer.AddAttribute("class", this.CssClass);
+            
+            var showOnEnterScript = new StringBuilder();
+            showOnEnterScript.Append("$refs.unmaskedInput.style='display: initial'");
+            showOnEnterScript.Append(";");
+            showOnEnterScript.Append("$refs.maskedInput.style='display: none'");
+
+            var hideOnLeave = new StringBuilder();
+            hideOnLeave.Append("$refs.maskedInput.style='display: initial'");
+            hideOnLeave.Append(";");
+            hideOnLeave.Append("$refs.unmaskedInput.style='display: none'");
+
+            writer.AddAttribute("x-on:mouseover", showOnEnterScript.ToString());
+            writer.AddAttribute("x-on:mouseout", hideOnLeave.ToString());
             writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
             var maskedInput = new HtmlInputPassword();
+            maskedInput.Attributes.Add("x-ref", "maskedInput");
             maskedInput.Attributes.Add("x-model", "Password");
-            maskedInput.ID = this.ClientID;
             maskedInput.Name = this.ClientID;
             maskedInput.Value = this.Password;
             maskedInput.RenderControl(writer);
 
             var unmaskedInput = new HtmlInputGenericControl();
+            unmaskedInput.Attributes.Add("x-ref", "unmaskedInput");
             unmaskedInput.Attributes.Add("x-model", "Password");
-            unmaskedInput.ID = this.ClientID;
+            unmaskedInput.Attributes.Add("style", "display : none");
+            unmaskedInput.Name = this.ClientID;
             unmaskedInput.Value = this.Password;
             unmaskedInput.RenderControl(writer);
 
