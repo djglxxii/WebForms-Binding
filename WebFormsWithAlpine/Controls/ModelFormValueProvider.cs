ee31da8b2f2892e92f0fc5b15656a50799cd5da2
David J. Gardner <djglxxii@outlook.com>
2021-05-22 10:24:35 -0400

    use extension method

--- WebFormsWithAlpine/Controls/ModelFormValueProvider.cs
+++ WebFormsWithAlpine/Controls/ModelFormValueProvider.cs
@@ -1,23 +1,19 @@
 ﻿using System;
 using System.Globalization;
-using System.Linq;
 using System.Web.ModelBinding;
-using System.Web.UI.WebControls;
+using WebFormsWithAlpine.Extensions;
 
 namespace WebFormsWithAlpine.Controls
 {
     public class ModelFormValueProvider<T> : IValueProvider where T : class, new()
     {
-        private readonly Type _modelType;
         private readonly ModelBindingExecutionContext _context;
         private readonly string _uniquePrefix;
 
         public ModelFormValueProvider(PageWithModel<T> page)
         {
-            var p = page.Parent;
-            _modelType = page.Model.GetType();
             _context = page.ModelBindingExecutionContext;
-            _uniquePrefix = GetUniquePrefix(page);
+            _uniquePrefix = page.GetUniquePrefix();
         }
         
         public bool ContainsPrefix(string prefix)
@@ -37,16 +33,6 @@ namespace WebFormsWithAlpine.Controls
                 rawValue: value,
                 attemptedValue: value ?? string.Empty,
                 culture: CultureInfo.InvariantCulture);
-        }
-
-        private static string GetUniquePrefix(PageWithModel<T> page)
-        {
-            var form = page.Form;
-            // TODO this will obviously fail if there are multiple ContentPlaceHolders.
-            // add support for specifying which is the 'main' ContentPlaceHolder.
-            var cp = form.Controls.OfType<ContentPlaceHolder>().Single();
-
-            return cp.UniqueID + page.IdSeparator;
         }
     }
 }
